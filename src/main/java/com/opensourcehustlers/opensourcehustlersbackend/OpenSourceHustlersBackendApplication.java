1d1fecbc7dd880d937ed906dbaa903dc99d24faf
Peter Ko <46854966+imko@users.noreply.github.com>
2024-03-06 12:03:22 -0800

    Set up Spring Boot project

---
+++ src/main/java/com/opensourcehustlers/opensourcehustlersbackend/OpenSourceHustlersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.opensourcehustlers.opensourcehustlersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSourceHustlersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSourceHustlersBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensourcehustlers/opensourcehustlersbackend/OpenSourceHustlersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensourcehustlers.opensourcehustlersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSourceHustlersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
