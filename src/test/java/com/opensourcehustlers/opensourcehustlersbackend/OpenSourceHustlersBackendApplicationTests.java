946f49de3eced5c0d10df57f78267b38cf7a433a
Peter Ko <46854966+imko@users.noreply.github.com>
2024-03-15 15:21:16 -0700

    Add tests for post repository layer

--- src/test/java/com/opensourcehustlers/opensourcehustlersbackend/OpenSourceHustlersBackendApplicationTests.java
+++ src/test/java/com/opensourcehustlers/opensourcehustlersbackend/OpenSourceHustlersBackendApplicationTests.java
@@ -3,6 +3,8 @@ package com.opensourcehustlers.opensourcehustlersbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class OpenSourceHustlersBackendApplicationTests {
